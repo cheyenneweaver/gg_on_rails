e3ccdc7f86b476d4c7e449e037567e2c121ccc82
Cheyenne Weaver <cheyenneweaver@gmail.com>
2012-07-30 18:41:37 -0500

    add a user with a birthday

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GgOnRails::Application.config.secret_token = 'b59ad134cd29d657a3dc04b1e21a332873692afd33e0198e2491dd82790daafe5e0657cfdf5a067205928d84c770242aa883c499eb1998bd3aaee7c3b57ede4b'
